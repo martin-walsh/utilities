72963a941e22189ac0f0f1da1ded21a13ce8d959
Martin Walsh <walshmartinpaul@gmail.com>
2016-07-15 11:47:21 +1000

    GIT config

---
+++ scripts/groovy/generateGitConfig.groovy
@@ -0,0 +1,39 @@
+#!/usr/bin/env groovy
+
+import groovy.util.ConfigSlurper
+
+println "Generating GIT Config..."
+
+def gitConfigFile = new File("${System.getProperty('user.home')}/.gitconfig")
+gitConfigFile.createNewFile()
+
+def config = new ConfigSlurper().parse(new File("${args[0]}/git-config.groovy").toURI().toURL())
+
+gitConfigFile.write """[user]
+  name = ${config.fullname}
+  email = ${config.email}
+[alias]
+  co = checkout
+  ci = commit
+  st = status -s
+  sb = show-branch
+  up = !git fetch origin && git rebase origin/master
+  lg = log --graph --pretty=format:'%Cred%h%Creset-%C(yellow)%d%Creset %f %Cgreen(%cr)%Creset'
+  who = log --format="'%ai %h %aN %s'"
+  ba = branch -a
+  rpo = remote prune origin
+  undo = reset HEAD~1 --soft
+[apply]
+  whitespace = nowarn
+[color]
+  ui = auto
+[branch]
+  autosetuprebase = always
+  autosetupmerge = true
+[showbranch]
+  default = --all
+[push]
+  default = current
+[log]
+  decorate = true
+"""
