7b17f6c5eb96b563287bca58a47084cd0f0ac20e
Martin Walsh <walshmartinpaul@gmail.com>
2016-09-01 16:13:00 +1000

    JDK upgrade

--- scripts/bash/initialize-java-version.sh
+++ scripts/bash/initialize-java-version.sh
@@ -1,7 +1,7 @@
 # Sets up JAVA_HOME & JDK_HOME environment variables
 export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 
-if [[ "$JAVA_HOME" == "/Library/Java/JavaVirtualMachines/jdk1.7.0_75.jdk/Contents/Home" ]]; then
+if [[ "$JAVA_HOME" == "/Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home" ]]; then
 	printf "Using Java 7...\n"
 
 elif [[ "$JAVA_HOME" == "/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home" ]]; then
