afbac23e06ab1d28e9a5afd98bd938f282716abb
Martin Walsh <walshmartinpaul@gmail.com>
2016-09-01 16:12:49 +1000

    More generalised naming pattern

--- scripts/bash/check-version.sh
+++ scripts/bash/check-version.sh
@@ -6,10 +6,10 @@ if [ -z ${#repoDir[@]} ]; then
 	printf "Empty directory\n"
 	exit 1
 fi
-if [[ -z `find . -type f -name 'Plugin*GrailsPlugin.groovy' -maxdepth 1` ]]; then
+if [[ -z `find . -type f -name '*GrailsPlugin.groovy' -maxdepth 1` ]]; then
 	grep 'app.version' application.properties | cut -d '=' -f 2
 else
-	grep 'def version' Plugin*GrailsPlugin.groovy | cut -d '=' -f 2 | cut -d '"' -f 2
+	grep 'def version' *GrailsPlugin.groovy | cut -d '=' -f 2 | cut -d '"' -f 2
 fi
 
 
