736c9294eabd6d83b8d84617e1dfea0ef43aba3c
Martin Walsh <walshmartinpaul@gmail.com>
2017-06-01 18:41:26 +1000

    use single quotes to prevent eval when profile is loaded

--- scripts/bash/docker-scripts.sh
+++ scripts/bash/docker-scripts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-alias sm-docker-login="echo 'Logging in to Docker ECR - 278521702583' && $(aws ecr get-login --registry-id=278521702583 --region us-west-2)"
+alias sm-docker-login='echo "Logging in to Docker ECR - 278521702583" && $(aws ecr get-login --registry-id=278521702583 --region us-west-2)'
