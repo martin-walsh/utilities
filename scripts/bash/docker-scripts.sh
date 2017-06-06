ce05406811e9a0a39b9b2a3832be16ac5a5d3560
Martin Walsh <walshmartinpaul@gmail.com>
2017-06-06 14:44:21 +1000

    More docker scripts

--- scripts/bash/docker-scripts.sh
+++ scripts/bash/docker-scripts.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 alias sm-docker-login='echo "Logging in to Docker ECR - 278521702583" && $(aws ecr get-login --registry-id=278521702583 --region us-west-2)'
+
+sm-describe-repos () {
+	aws ecr describe-repositories --registry-id=278521702583 --repository-names=$1
+}
+
+sm-describe-images() {
+	aws ecr describe-images --registry-id=278521702583 --repository-name $1
+}
