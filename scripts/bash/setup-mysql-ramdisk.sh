e69c5bd406bed97918b9c5cdb0106986be03b842
Martin Walsh <walshmartinpaul@gmail.com>
2017-06-26 16:36:00 +1000

    Set timezone for dbs

--- scripts/bash/setup-mysql-ramdisk.sh
+++ scripts/bash/setup-mysql-ramdisk.sh
@@ -4,6 +4,9 @@ docker run --name aridb --tmpfs /var/lib/mysql:rw --rm -d -p 3307:3306 278521702
 
 docker run --name cmdb --tmpfs /var/lib/mysql:rw --rm -d -p 3306:3306 278521702583.dkr.ecr.us-west-2.amazonaws.com/cm/cm-test-db:mysql_5.6_cm_master
 
+docker exec aridb bash -c "ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime"
+docker exec cmdb bash -c "ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime"
+
 until docker exec cmdb mysqladmin --silent ping; do
   echo "Waiting for mysqld to start. Retry in 5s..."
   sleep 5
