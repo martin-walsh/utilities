6f7e02bb7c2dcb1e285c8417ad8d4af8b0b2cb33
Martin Walsh <walshmartinpaul@gmail.com>
2017-07-07 17:08:10 +1000

    Fix docker login. stupid aws...

--- scripts/bash/docker-scripts.sh
+++ scripts/bash/docker-scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias sm-docker-login='echo "Logging in to Docker ECR - 278521702583" && $(aws ecr get-login --registry-id=278521702583 --region us-west-2)'
+alias sm-docker-login='echo "Logging in to Docker ECR - 278521702583" && $(aws ecr get-login --registry-id=278521702583 --region us-west-2 | sed "s/-e none//g")'
 
 sm-describe-repos () {
 	aws ecr describe-repositories --registry-id=278521702583 --repository-names=$1
