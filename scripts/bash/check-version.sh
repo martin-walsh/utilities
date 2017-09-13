31ce2925b4533556ca7d7bbb4b6c2d0f8b1b3a60
Martin Walsh <walshmartinpaul@gmail.com>
2017-09-13 15:23:31 +1000

    handle whitespace

--- scripts/bash/check-version.sh
+++ scripts/bash/check-version.sh
@@ -10,7 +10,7 @@ if [[ -z `find . -type f -name '*GrailsPlugin.groovy' -maxdepth 1` ]]; then
 	if [[ -z `find . -type f -name 'build.gradle' -maxdepth 1` ]]; then
 		grep 'app.version' application.properties | cut -d '=' -f 2
 	else
-		egrep -m 1 '^\s+version' build.gradle | egrep -o '[0-9.A-Z]+'
+		egrep -m 1 '^\s*version' build.gradle | egrep -o '[0-9.A-Z]+'
 	fi
 else
 	grep 'def version' *GrailsPlugin.groovy | cut -d '=' -f 2 | cut -d '"' -f 2
