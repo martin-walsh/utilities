2c3bcbc3e3665debb64f9986180de7129dc0def4
Martin Walsh <walshmartinpaul@gmail.com>
2018-03-13 11:34:42 +1100

    Add spring boot wrappers & completion

---
+++ envs/deploy.sh
@@ -0,0 +1,15 @@
+#/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+	echo "Usage: deploy.sh uk01"
+	exit 1
+fi
+
+echo "Deploying aliases & completion etc to $1"
+
+script_dir=$(dirname "$0")
+
+pushd $script_dir > /dev/null
+scp .inputrc $1:~
+scp bash_aliases $1:~
+popd > /dev/null
--- scripts/bash/test-report.sh
+++ scripts/bash/test-report.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-open target/test-reports/html/failed.html
+if [ -f target/test-reports/html/failed.html ]; then
+    open target/test-reports/html/failed.html
+fi
+
+if [ -f build/reports/tests/test/index.html ]; then
+    open build/reports/tests/test/index.html
+fi
